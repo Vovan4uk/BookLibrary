c0ce4cdc99446b69aac17126965762d3839cd5ae
vperep <Vovan4uk86@gmail.com>
2015-11-13 09:21:38 +0200

    test Validator

--- BookLibrary-cdi/src/main/java/ua/softserve/booklibrary/servlet/AddServlet.java
+++ BookLibrary-cdi/src/main/java/ua/softserve/booklibrary/servlet/AddServlet.java
@@ -10,12 +10,20 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Date;
+import java.util.Set;
 
 @WebServlet ("/add")
 public class AddServlet extends HttpServlet {
+
+    private static ValidatorFactory vf;
+    private static Validator validator;
 
     @EJB
     ReviewService reviewService;
@@ -24,9 +32,12 @@ public class AddServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
 
+        vf = Validation.buildDefaultValidatorFactory();
+        validator = vf.getValidator();
+
         Review review = new Review();
         review.setCommentBody("new test review body");
-        review.setCommenterName("commenter name");
+        review.setCommenterName("commenter name test validation");
         review.setCommentCreate(new Date());
         review.setCommentRating(3);
 
@@ -34,10 +45,15 @@ public class AddServlet extends HttpServlet {
         book.setId(1L);
         review.setBook(book);
 
-        reviewService.saveReview(review);
-
-        PrintWriter out = resp.getWriter();
-        out.print("New Object added "+review);
+        Set<ConstraintViolation<Review>> violations = validator.validate(review);
+        if (violations.size() > 0) {
+            PrintWriter out = resp.getWriter();
+            out.print(violations);
+        } else {
+            reviewService.saveReview(review);
+            PrintWriter out = resp.getWriter();
+            out.print("New Object added " + review);
+        }
 
     }
 
