5e8cfa71e9de63330bcc579ca974e6034fbfc50e
vperep <Vovan4uk86@gmail.com>
2016-01-08 17:57:03 +0200

    update arquillian tests some fix

--- BookLibrary-cdi/src/test/java/ua/softserve/booklibrary/arquillian/AuthorTest.java
+++ BookLibrary-cdi/src/test/java/ua/softserve/booklibrary/arquillian/AuthorTest.java
@@ -9,19 +9,13 @@ import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.jboss.shrinkwrap.resolver.api.maven.Maven;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.testng.Assert;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import ua.softserve.booklibrary.bean.AuthorAction;
 import ua.softserve.booklibrary.bean.filter.BooksFilteringBean;
 import ua.softserve.booklibrary.bean.sort.MainSortingBean;
 import ua.softserve.booklibrary.dao.facade.AuthorFacade;
-import ua.softserve.booklibrary.dao.facade.impl.AuthorFacadeImpl;
 import ua.softserve.booklibrary.dao.home.AuthorHome;
-import ua.softserve.booklibrary.dao.home.impl.AuthorHomeImpl;
 import ua.softserve.booklibrary.entity.Author;
 import ua.softserve.booklibrary.exception.LibraryException;
 import ua.softserve.booklibrary.manager.AuthorManager;
@@ -29,14 +23,8 @@ import ua.softserve.booklibrary.rest.client.AuthorClientService;
 import ua.softserve.booklibrary.rest.service.AuthorService;
 
 import javax.ejb.EJB;
-import javax.inject.Inject;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
+import javax.ejb.EJBException;
 import java.io.File;
-import java.util.ArrayList;
-
-import static org.mockito.Mockito.mock;
-
 
 public class AuthorTest extends Arquillian {
 
@@ -49,17 +37,6 @@ public class AuthorTest extends Arquillian {
 	@EJB
 	private AuthorManager authorManager;
 
-/*
-	@PersistenceContext
-	protected EntityManager em;
-
-	@EJB
-	private AuthorService authorService;
-*/
-
-	@Inject
-	private AuthorClientService authorClientService;
-
 	@Deployment
 	public static Archive<?> createTestArchive() {
 
@@ -68,12 +45,6 @@ public class AuthorTest extends Arquillian {
 		File[] richfacesLibs = Maven.resolver().loadPomFromFile("pom.xml").importRuntimeDependencies()
 				.resolve("org.richfaces:richfaces:4.5.10.Final")
 				.withTransitivity().asFile();
-
-/*
-		File[] mocktoLibs = Maven.resolver().loadPomFromFile("pom.xml").importRuntimeDependencies()
-				.resolve("org.mockito:mockito-all:1.10.19")
-				.withTransitivity().asFile();
-*/
 
 		war.addPackages(true, Author.class.getPackage());
 		war.addPackages(true, AuthorHome.class.getPackage());
@@ -86,84 +57,94 @@ public class AuthorTest extends Arquillian {
 		war.addClass(BooksFilteringBean.class);
 		war.addClass(LibraryException.class);
 		war.addAsLibraries(richfacesLibs);
-/*
-		war.addAsLibraries(mocktoLibs);
-*/
 		war.addAsResource("test-persistence.xml", "META-INF/persistence.xml");
 		war.addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
 
-		System.out.println(war.toString(true));
 		return war;
 	}
 
-/*
-		@BeforeClass
-		public void setUp() {
-			em = mock(EntityManager.class);
-			MockitoAnnotations.initMocks(this);
-		}
-*/
-
-	@Test
-	@UsingDataSet("dataset/initial-authors-dataset.xml")
-	public void authorRestTest() {
-		//Create new Author
-		Author author = new Author();
-		author.setFirstName("TestFirstName");
-		author.setSecondName("TestSecondName");
-/*
-		authorHome.setEm(em);
-*/
-		//Persist new Author by Rest WS
-//		authorClientService.saveAuthor(author);
-
-		authorManager.save(author);
-		//Create another Author
-		Author anotherAuthor = new Author();
-		anotherAuthor.setFirstName("TestAnotherFirstName");
-		anotherAuthor.setSecondName("TestAnotherSecondName");
-		//Persist another Author by Rest WS
-		authorClientService.saveAuthor(anotherAuthor);
-
-		//Check count Author entities in database
-		Assert.assertEquals(3, authorFacade.findAll().size());
-	}
-
-/*
+	/*Simple DAO test*/
 	@Test
 	public void authorDAOTest(){
 		//Create new Author
 		Author author = new Author();
-		author.setFirstName("TestFirstName");
-		author.setSecondName("TestSecondName");
+		author.setFirstName("TestName");
+
 		//Persist new Author
-		Author result = authorHome.save(author);
+		authorHome.save(author);
+
 		//Check id after persist
-		Assert.assertNotNull(result.getId());
+		Assert.assertNotNull(author.getId());
 
 		//Create another Author
 		Author anotherAuthor = new Author();
-		anotherAuthor.setFirstName("TestAnotherFirstName");
-		anotherAuthor.setSecondName("TestAnotherSecondName");
+		anotherAuthor.setFirstName("TestAnotherName");
+
 		//Persist another Author
 		authorHome.save(anotherAuthor);
 
-		//Check count Author entities in database
+		//Check count Authors in database
 		Assert.assertEquals(2, authorFacade.findAll().size());
-	}
-*/
 
-/*
+		//Change first name
+		author.setFirstName("ChangedName");
+
+		//Update Author
+		authorHome.update(author);
+
+		//Check if update successful
+		Assert.assertEquals("ChangedName", authorFacade.findByPk(author.getId()).getFirstName());
+
+		//Delete Author
+		authorHome.removeByPk(author.getId());
+
+		//Check count Authors in database
+		Assert.assertEquals(1, authorFacade.findAll().size());
+	}
+
+	/*Test save Author with Arquillian persistence DBUnit through Manager(business level)*/
 	@Test
-	@UsingDataSet("dataset/initial-authors-dataset.xml")
-	@ShouldMatchDataSet(value = "dataset/after-save-authors-dataset.xml",
+	@UsingDataSet("datasets/initial-authors-dataset.xml")
+	@ShouldMatchDataSet(value = "datasets/after-save-authors-dataset.xml",
 			excludeColumns = {"ID", "CREATE_DATE"},
-			orderBy = {"AUTHOR.FIRST_NAME"})
+			orderBy = {"FIRST_NAME"})
 	public void saveAuthorTest() throws Exception {
 		Author author = new Author();
-		author.setFirstName("John");
-		author.setSecondName("Cena");
-		authorHome.save(author);
+		author.setFirstName("Monk");
+		author.setSecondName("Simon");
+		authorManager.save(author);
 	}
-*/
+
+	/*Test update Author with Arquillian persistence DBUnit through Manager(business level)*/
+	@Test
+	@UsingDataSet("datasets/initial-authors-dataset.xml")
+	@ShouldMatchDataSet(value = "datasets/after-update-authors-dataset.xml",
+			excludeColumns = {"ID", "CREATE_DATE"},
+			orderBy = {"FIRST_NAME"})
+	public void updateAuthorTest() throws Exception {
+		Author author = authorManager.findByPk(1000L);
+		author.setFirstName("Lafferty");
+		author.setSecondName("Mur");
+		authorManager.update(author);
+	}
+
+	/*Test delete Author with Arquillian persistence DBUnit through Manager(business level)*/
+	@Test
+	@UsingDataSet("datasets/initial-authors-dataset.xml")
+	@ShouldMatchDataSet(value = "datasets/after-delete-authors-dataset.xml",
+			excludeColumns = {"ID", "CREATE_DATE"},
+			orderBy = {"FIRST_NAME"})
+	public void deleteAuthorTest() throws Exception {
+		authorManager.removeByPk(1000L);
+	}
+
+	/*Test save not unique Author with Arquillian persistence DBUnit through Manager(business level)*/
+	@Test(expectedExceptions = EJBException.class)
+	@UsingDataSet("datasets/initial-authors-dataset.xml")
+	public void saveNotUniqueAuthorTest() throws Exception {
+		Author author = new Author();
+		author.setFirstName("Lafore");
+		author.setSecondName("Robert");
+		authorManager.save(author);
+	}
 }
